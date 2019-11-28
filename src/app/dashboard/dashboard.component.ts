bc23855b17621e1ee90e673c58be31c46b96ef1d
Ajeky <alvaromarquez98@gmail.com>
2019-11-28 14:32:58 +0100

    Pagina estadisticas en marcha

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto.interface';
+import { ProductoService } from '../services/producto.service';
+import { monkeyPatchChartJsTooltip, monkeyPatchChartJsLegend, Label, SingleDataSet } from 'ng2-charts';
+import { ChartOptions, ChartType } from 'chart.js';
+import { CajaService } from '../services/caja.service';
+import { EntidadService } from '../services/entidad.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  total: number;
+  
+  //OPCIONES GENÉRICAS PARA LAS GRÁFICAS
+  public opcionesGraficaResponsiva: ChartOptions = {
+    responsive: true,
+  };
+  public opcionesGraficaNoResponsiva: ChartOptions = {
+    responsive: false,
+  };
+  public tipoGraficaTarta: ChartType = 'pie';
+  public tipoGraficaBarra: ChartType = 'bar';
+  public leyendaGraficaTrue = true;
+  public leyendaGraficaFalse = false;
+  public pluginsGraficaVoid = [];
+  
+  //OPCIONES GRÁFICA DE KILOS POR PRODUCTO TIPO TARTA  
+  public etiquetasGraficaTartaKilosPorProducto: Label[];
+  public valoresGraficaTartaKilosPorProducto: SingleDataSet[];
+  graficaTartaKilosPorProductoReady = false;
+
+  //OPCIONES GRÁFICA DE KILOS POR ENTIDAD TIPO BARRA
+  public etiquetasGraficaBarraKilosPorEntidad: Label[];
+  public valoresGraficaBarraKilosPorEntidad: SingleDataSet[];
+  graficaBarraKilosPorEntidadReady = false;
+
+  constructor (
+    private productoServicio: ProductoService,
+    private cajaServicio: CajaService,
+    private entidadServicio: EntidadService
+  ) {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit')
+    this.cargarDatosGraficaTartaKilosPorProductos();
+    // this.cargarDatosGraficaBarrasKilosPorEntidad()
+  }
+
+  cargarDatosGraficaTartaKilosPorProductos() {
+    console.log('Cargando productos')
+    this.productoServicio.getProductosRaw().subscribe(resp => {
+      this.etiquetasGraficaTartaKilosPorProducto = [];
+      this.valoresGraficaTartaKilosPorProducto = [];
+
+      resp.forEach((producto: any) => {
+        this.etiquetasGraficaTartaKilosPorProducto.push(producto.nombreProducto);
+        this.valoresGraficaTartaKilosPorProducto.push(producto.kilos);
+      })
+      this.graficaTartaKilosPorProductoReady = true;
+      console.log(this.etiquetasGraficaTartaKilosPorProducto);
+      console.log(this.valoresGraficaTartaKilosPorProducto);
+    });
+  }
+
+  // cargarDatosGraficaBarrasKilosPorEntidad() {
+  //   this.entidadServicio.getEntidadesRaw().subscribe(resp => {
+  //     this.etiquetasGraficaBarraKilosPorEntidad = [];
+  //     this.valoresGraficaBarraKilosPorEntidad = [];
+
+  //     resp.forEach((entidad: any) => {
+  //       this.total = 0;
+  //       this.etiquetasGraficaBarraKilosPorEntidad.push(entidad.nombre);
+  //       this.cajaServicio.getCajasByEntidad(entidad).forEach((caja: any) => {
+  //         this.total += caja.kilos;
+  //       });
+  //       this.valoresGraficaBarraKilosPorEntidad.push(this.total);
+  //     });
+  //   });
+  //}
+
+
+  
 }
--- src/app/services/caja.service.ts
+++ src/app/services/caja.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 import { CajaDto } from '../models/caja.dto';
 import { Producto } from '../models/producto.interface';
 import { Caja } from '../models/caja.interface';
+import { Entidad } from '../models/entidad.interface';
 
 const nombreColeccion = 'cajas';
 
@@ -21,12 +22,21 @@ export class CajaService {
     return this.db.collection<Caja>(nombreColeccion).snapshotChanges();
   }
 
+  public getCajasRaw() {
+    return this.db.collection<Caja>(nombreColeccion).valueChanges();
+  }
+
   public updateCaja(id: string, caja: Caja) {
     return this.db.collection<Caja>(nombreColeccion).doc(id).update(caja);
   }
 
   public deleteCaja(id: string) {
     return this.db.collection<Caja>(nombreColeccion).doc(id).delete();
+  }
+
+  public getCajasByEntidad(entidad: Entidad) {
+    return this.db.collection<Caja>(nombreColeccion, ref =>
+      ref.where('entidad', '==', entidad)).valueChanges();
   }
 
 }
